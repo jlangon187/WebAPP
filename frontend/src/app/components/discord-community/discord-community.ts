5141c2b5b834fd22097bd932efb394a04f8ed1c8
Javi <javiliyors@gmail.com>
2026-05-03 16:20:27 +0200

    Docker 0.8.5 Limpieza

--- frontend/src/app/components/discord-community/discord-community.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-discord-community',
-  imports: [],
-  templateUrl: './discord-community.html',
-  styleUrl: './discord-community.css',
-})
-export class DiscordCommunity {}
